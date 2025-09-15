e67919e84c1b51415a4b2f6e74e9aa1933f49182
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-15 18:17:36 +0530

    refactor: Simplify order schema by removing redundant properties from items

--- backend/src/models/Order.js
+++ backend/src/models/Order.js
@@ -5,21 +5,13 @@ const orderSchema = mongoose.Schema(
     {
         userId : {type: mongoose.Schema.Types.ObjectId, required: true},
         items: [
-                {
-                productId: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "Product",
-                    required: true
-                },
-                qty: {
-                    type: Number,
-                    required: true,
-                    min: 1,
-                    default: 1
-                }
-                }
-            ],
+          {
+            productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+            qty: { type: Number, required: true }
+          }
+        ],
         total : {type:Number, required: true},
+
     },
     { timestamps: true }
 );
