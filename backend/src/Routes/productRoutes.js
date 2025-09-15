1be05ca447786508a9a8e78b23b028ee01621701
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-15 17:11:18 +0530

    fix: Update addItem_Product route to remove unnecessary ID parameter

--- backend/src/Routes/productRoutes.js
+++ backend/src/Routes/productRoutes.js
@@ -4,7 +4,7 @@ import { addItem_Product, deleteItem_Product, getItems_Product, updateItem_Produ
 const router = express.Router()
 
 router.get("/",getItems_Product)
-router.post("/add/:id",addItem_Product)
+router.post("/add",addItem_Product)
 router.delete("/delete/:id",deleteItem_Product)
 router.put("/update/:id",updateItem_Product)
 
