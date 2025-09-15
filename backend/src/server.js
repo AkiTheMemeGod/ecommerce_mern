f7931248c745200e74032ee4fc53ec0053e96baa
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-15 17:45:20 +0530

    fix: Add CORS middleware to enable cross-origin requests

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
 import cartRoutes from './Routes/cartRoutes.js';
 import authRoutes from './Routes/authRoutes.js'
 import productRoutes from './Routes/productRoutes.js'
 import connectDB from './config/db.js';
+
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/cart", cartRoutes);
