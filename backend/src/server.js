b282cae02bf89c0f05142d999cee98e064652568
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-15 18:17:29 +0530

    refactor: Reorganize server.js to improve route imports and enable order routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,21 @@ import cors from 'cors';
 
 import cartRoutes from './Routes/cartRoutes.js';
 import authRoutes from './Routes/authRoutes.js'
-import productRoutes from './Routes/productRoutes.js'
+import productRoutes from './Routes/productRoutes.js';
+import orderRoutes from './Routes/orderRoutes.js';
 import connectDB from './config/db.js';
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/api/cart", cartRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/order", orderRoutes);
 
 //app.use("/api/orders", orderRoutes);
 
