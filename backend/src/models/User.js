d64d214a835098106a6abbd8a191c4d044606d6e
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-15 17:45:24 +0530

    fix: Remove required role field from user schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -6,10 +6,9 @@ const userSchema = mongoose.Schema(
         name : {type:String, required: true},
         email : {type:String, required: true},
         passwordHash : {type:String, required: true},
-        role : {type:String, required: true},
     }
 );
-
+ 
 const User = mongoose.model("User",userSchema);
 
 export default User;
