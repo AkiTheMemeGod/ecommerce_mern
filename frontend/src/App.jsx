7327f44e8bbaf922ac8b0a15709cfe747266fe59
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-16 13:08:52 +0530

    feat: add CartPage route to application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import AuthPage from "./pages/AuthPage.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
+import CartPage from "./pages/CartPage.jsx";
 
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem("token");
@@ -13,7 +14,9 @@ const App = () => {
     <div className="relative h-full w-full">
       <Routes>
         <Route path="/" element={<AuthPage />} />
-        <Route path="/product/:id" element={ProductPage}/>
+        <Route path="/product/:id" element={<ProductPage/>}/>
+        <Route path="/viewcart" element={<CartPage/>}/>
+
         <Route
           path="/home"
           element={
