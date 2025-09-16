0fcf1b68cac7f526d4d98937dd056a974a90c8a5
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-16 11:45:50 +0530

    feat: implement main application structure with routing and private route handling

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import AuthPage from "./pages/AuthPage.jsx";
+import ProductPage from "./pages/ProductPage.jsx";
+
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/" />;
+};
+
+const App = () => {
+  return (
+    <div className="relative h-full w-full">
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/product/:id" element={ProductPage}/>
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <HomePage />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
