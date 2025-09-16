73deee734d3e31c1836f695e0356fb9797da6b44
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-16 13:09:02 +0530

    feat: add View Cart button to NavBar for easy access to cart items

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -10,9 +10,14 @@ const NavBar = () => {
     navigate("/"); // redirect to login
     toast.success("Logged out successfully");
     };
+
+    const handleViewcart = () => {
+    navigate("/viewcart");
+    }
   return (
-    <div className="w-full bg-base-100 shadow-md px-6 py-4 flex justify-between items-center">
+    <div className="w-full bg-base-100 shadow-md px-6 py-4 flex justify-between">
         <h1 className="text-xl font-bold text-primary">My Store 🛍️</h1>
+        <button className="btn btn-secondary" onClick={handleViewcart}>View Cart</button>
         <button onClick={handleLogout} className="btn btn-error">
           Logout
         </button>
